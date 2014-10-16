29dd034a3f9db16f267aa637e463214da0dc7868
Lukas Mueller <lam87@cornell.edu>
2014-10-16 19:33:06 -0400

    restructure script.

--- call_genotypes.pl
+++ call_genotypes.pl
@@ -1,16 +1,17 @@
 
-
 use strict;
 
 use Getopt::Long;
-use Math::BigInt;
+
+use lib '/home/mueller/cxgn/gbs/lib';
+
+use StatFunc;
 
 our $MIN_DEPTH= 0;
 our $MAF = 0.001;
 our $OUTFILE = "";
 
 GetOptions("maf=i" => \$MAF,
-	   "min_depth=i" => \$MIN_DEPTH,
 	   "outfile=s" => \$OUTFILE,
     );
 
@@ -21,15 +22,84 @@ if (! $OUTFILE) {
 }
 
 my $stats_file = $OUTFILE.".stats";
+my @clone_names;
+my @clone_info; 
+my @bad_calls_per_clone;
+
+print STDERR "Detecting bad clones...\n";
+
+# Filter bad clones. Keep a list of clones that failed in @clone_info
+#
+my $snps_processed = 0;
+
+open (my $F, "<", $filename) || die "Can't open file $filename\n";
+my $ignore_clones_count = 0;
+
+while(<$F>) { 
+    chomp;
+    if ($snps_processed % 100 == 0) { 
+	print STDERR "Processing $snps_processed ...\n";
+    }
+    
+    if (m/\#\#/) { 
+	next;
+    }
+    
+    if (/^#CHROM/ || /^CHROM/ ) { 
+	my @header = split /\t/;
+	@clone_names = @header[9.. $#header];	
+    }
+
+    my ($chr, $position, $snp_id, $ref_allele, $alt_allele, $qual, $filter, $info, $format,  @snps) = split /\t/;
+    
+    my $depth = 0;
+
+    my @counts;
+    my $ignore_clones_count = 0;
+
+    for (my $n = 0; $n < @clone_names; $n++) { 
+	
+	if ($clone_names[$n] =~ /blank/i || $clone_names[$n] =~ /failed/i || $clone_names[$n] =~/empty/i || $clone_names[$n] =~ /NA\:\d+/i) { 
+	    $clone_info[$n] = "B";  # remember this as a blank (do not count in stats)
+	    $ignore_clones_count++;
+	}
+	else { 
+	    $clone_info[$n] = "";
+	    
+	    # find allele frequencies by adding up the counts for each allele
+	    #
+	    my ($c1, $c2) = StatFunc::get_counts($snps[$n]);
+	    if ($c1 + $c2 < 2) { 
+		$bad_calls_per_clone[$n]++;
+	    }
+	}
+    }
+
+    $snps_processed++;
+}
+
+for( my $n = 0; $n< @bad_calls_per_clone; $n++) { 
+    if ($bad_calls_per_clone[$n] >  $snps_processed * 0.95) { 
+	$clone_info[$n] = 'F';
+	print STDERR "Throwing away clone $clone_names[$n] (bad calls per clone = $bad_calls_per_clone[$n]\n";
+    }
+}
+
+close($F);
+
+$MIN_DEPTH = (@clone_names - $ignore_clones_count) * 1.5; 
+print STDERR "Using a default of $MIN_DEPTH\n";
+
+$snps_processed = 0;
 
 open(my $F, "<", $filename) || die "Can't open file $filename\n";
 open(my $OUT, ">", $OUTFILE) || die "Can't open outfile $OUTFILE\n";
 open(my $STATS, ">", $stats_file) || die "Can't open stats file $stats_file\n";
 
-my @clone_names;
+my @clone_failed_count;
+
 my %data;
 
-my $snps_processed = 0;
 
 while (<$F>) { 
     chomp;
@@ -47,95 +117,126 @@ while (<$F>) {
     if (/^#CHROM/ || /^CHROM/ ) { 
 	my @header = split /\t/;
 	@clone_names = @header[9.. $#header];	
-	if (! $MIN_DEPTH) { $MIN_DEPTH = scalar(@clone_names) * 2; }
-	print STDERR "Using MIN_DEPTH of $MIN_DEPTH\n";
 	print STDERR "Using MAF of $MAF\n";
+	
+	print $OUT "snps";
+
+	print STDERR "Printing header information...\n";
+	for (my $i=0; $i<@clone_names; $i++) { 
+	    if (!$clone_info[$i]) { 
+		print $OUT "\t".$clone_names[$i];  
+	    }
+	}
+	print $OUT "\n";
     }
     
+    
+
     else { 
-	my ($chr, $position, $snp_id, $ref_allele, $alt_allele, $qual, $filter, $info, $format, $x, @snps) = split /\t/;
+	my ($chr, $position, $snp_id, $ref_allele, $alt_allele, $qual, $filter, $info, $format,  @snps) = split /\t/;
 	
-	#print STDERR "SNP: $snp_id\t$ref_allele\t$alt_allele\t$qual\n";
+	print STDERR "SNP: $snp_id\t$ref_allele\t$alt_allele\t$qual\n";
 	my $depth = 0;
 	if ($info =~ /DP=(\d+)$/) { 
 	    $depth = $1;
-	    #print STDERR "DEPTH: $depth\n";
+	    print STDERR "DEPTH: $depth\n";
 	}
 	
 	my $total_c1 = 0;
 	my $total_c2 = 0;
 	
-	if ($depth > $MIN_DEPTH) { 
-	    my @counts;
-	    for (my $n = 0; $n < @clone_names; $n++) { 
-		
-		if ($clone_names[$n]!~/blank/i) { 
-		    #print "PROCESSING $snp_id CLONE NAME: $clone_names[$n]\n";
-		    
-		    my ($c1, $c2) = get_counts($snps[$n]);
-		    $counts[$n] = [ $c1, $c2 ];
-		    $total_c1 += $c1;
-		    $total_c2 += $c2;
-		}
-	    }	    
-	    my $allele_freq = $total_c1 / ($total_c1 + $total_c2);
+	if ($depth < $MIN_DEPTH) { 
+	    print STDERR "Skipping $snp_id because of $depth < $MIN_DEPTH\n";
+	    next();
+	}
 
-	    if ($allele_freq > (1 - $MAF) || $allele_freq < $MAF) { 
-		#print STDERR "Skipping ALLELE FREQ = $allele_freq\n";
-		next();
-	    }
+	my @counts;
 
-	    my $pAA = $allele_freq **2;
-	    my $pAB = $allele_freq * (1 - $allele_freq) * 2 ;
-	    my $pBB = (1 - $allele_freq) **2;
+	for (my $n = 0; $n < @clone_names; $n++) { 
 
-	    my @dosages; 
-
-	    for (my $n = 0; $n < @clone_names; $n++) { 
-		if (ref($counts[$n]) eq "ARRAY") {
-		    if ( ($counts[$n]->[0] + $counts[$n]->[1]) > 1 ) { 
-			$dosages[$n] = get_genotype($pAA, $pAB, $pBB, $counts[$n]->[0], $counts[$n]->[1]);
-		    }
-		    else { $dosages[$n] = undef; }
-		}
-	    }
-
-	    # hardy weinberg filter
-	    my %score = hardy_weinberg_filter(@dosages);
-
-	    printf($STATS "$snp_id\t%.3f\t%.4f\t%.1f", $score{scored_marker_fraction}, $score{allele_freq}, $score{chi});
-
-	    if ($score{scored_marker_fraction} < 0.4) { 
-		print STDERR "Skipping $snp_id because of low scored markers ($score{scored_marker_fraction})\n";
-		print $STATS "\tSKIPPED\n";
-		next();
-	    }
-	    if (($score{allele_freq} > 1-$MAF) || ($score{allele_freq} < $MAF) ) { 
-		print STDERR "Skipping $snp_id because of high allele frequency ($score{allele_freq})\n";
-		print $STATS "\tSKIPPED\n";
-		next();
-	    }
-	    if ($score{chi} > 20) { 
-		print STDERR "Skipping $snp_id because of Hardy Weinberg distribution ($score{chi})\n";
-		print $STATS "\tSKIPPED\n";
-		next();
-	    }
-	    print $STATS "\tRETAINED\n";
+	    # find allele frequencies by adding up the counts for each allele
+	    #
+	    my ($c1, $c2) = StatFunc::get_counts($snps[$n]);
+	    $counts[$n] = [ $c1, $c2 ];
+	    $total_c1 += $c1;
+	    $total_c2 += $c2;
 	    
-	    print $OUT $snp_id;
-	    for (my $n = 0; $n < @clone_names; $n++) { 
-		if (ref($counts[$n]) eq "ARRAY") {
-		    if ( ($counts[$n]->[0] + $counts[$n]->[1]) >1 ) { 
-			print $OUT "\t";
-			printf $OUT "%.2f", $dosages[$n];
-		    }
-		    else { print $OUT "\tNA"; }
+	    # keep a sum of failed snps for each clone
+	    #
+	    if ($c1 == 0 && $c2 == 0) { 
+		$clone_failed_count[$n]++; 
+		####$ignore_clones_count++;
+	    }
+	}	    
+	my $allele_freq = $total_c1 / ($total_c1 + $total_c2);
+	
+	if ($allele_freq > (1 - $MAF) || $allele_freq < $MAF) { 
+	    print STDERR "Skipping ALLELE FREQ = $allele_freq\n";
+	    next();
+	}
+	
+	my $pAA = $allele_freq **2;
+	my $pAB = $allele_freq * (1 - $allele_freq) * 2 ;
+	my $pBB = (1 - $allele_freq) **2;
+	
+	my @dosages; 
+	
+	# calculate the genotype using the counts and some stats.
+	#
+	for (my $n = 0; $n < @clone_names; $n++) { 
+	    if ($clone_info[$n] eq "") { # clone is ok (not monomorphic or blank)
+		if ( ($counts[$n]->[0] + $counts[$n]->[1]) > 1 ) { 
+		    $dosages[$n] = StatFunc::get_genotype($pAA, $pAB, $pBB, $counts[$n]->[0], $counts[$n]->[1]);
+		}
+		else { $dosages[$n] = undef; }
+	    }
+	}
+	
+	# hardy weinberg filter
+	my %score = StatFunc::hardy_weinberg_filter(\@dosages, $ignore_clones_count);
+	
+	if (exists($score{invalid}) && $score{invalid} == 1) { 
+	    print STDERR "Skipping monomorphic marker $snp_id\n";
+	    next();
+	}
+	printf($STATS "$snp_id\t%.3f\t%.4f\t%.1f", $score{scored_marker_fraction}, $score{allele_freq}, $score{chi});
+	
+	if ($score{scored_marker_fraction} < 0.4) { 
+	    print STDERR "Skipping $snp_id because of low scored markers ($score{scored_marker_fraction})\n";
+	    print $STATS "\tSKIPPED\n";
+		next();
+	}
+	#if (($score{allele_freq} > 1-$MAF) || ($score{allele_freq} < $MAF) ) { 
+	#    print STDERR "Skipping $snp_id because of high allele frequency ($score{allele_freq})\n";
+	#    print $STATS "\tSKIPPED\n";
+	#    next();
+	#}
+        if ($score{heterozygote_count} < 3) { 
+	    print STDERR "Skipping $snp_id because of low heterzygote count.\n";
+	    print $STATS "\tSKIPPED\n";
+	    next();
+	}
+	if ($score{chi} > 20) { 
+	    print STDERR "Skipping $snp_id because of Hardy Weinberg distribution ($score{chi})\n";
+	    print $STATS "\tSKIPPED\n";
+	    next();
+	}
+	print $STATS "\tRETAINED\n";
+	    
+	print $OUT $snp_id;
+
+	# output the snp
+	#
+	for (my $n = 0; $n < @clone_names; $n++) { 
+	    if ($clone_info[$n] eq "") {
+		if ( ($counts[$n]->[0] + $counts[$n]->[1]) >1 ) { 
+		    print $OUT "\t";
+		    printf $OUT "%.2f", $dosages[$n];
 		}
 		else { print $OUT "\tNA"; }
 	    }
-	    print $OUT "\n";
-
 	}
+	print $OUT "\n";	
     }
 }
 
@@ -143,92 +244,14 @@ close($F);
 close($OUT);
 close($STATS);
 
-sub get_counts { 
-    my $snp = shift;
-    
-    my $counts = (split /\:/, $snp)[1];
+open(my $F, ">", $filename.".failed_clones") || die "Can't open $filename\n";
 
-    my ($c1, $c2) = split /\,/, $counts;
-
-    return ($c1, $c2);
-
-}
-
-sub get_genotype { 
-    my $pAA = shift;
-    my $pAB = shift;
-    my $pBB = shift;
-    my $c1 = shift;
-    my $c2 = shift;
-
-    my $n = $c1 + $c2;
-
-    my $N = Math::BigInt->new($n);
-
-    my $pDAA = ($N->bnok($c1))->numify * 0.99 ** $c1 * 0.01 ** $c2;
-    my $pDAB = ($N->bnok($c1))->numify * 0.5 ** $c1 * 0.5 ** $c2;
-    my $pDBB = ($N->bnok($c2))->numify * 0.01 ** $c1 * 0.99 ** $c2;
-
-    #print STDERR "pDAA: $pDAA, pDAB $pDAB, pDBB $pDBB\n";
-
-    my $pSAA = $pDAA * $pAA;
-    my $pSAB = $pDAB * $pAB;
-    my $pSBB = $pDBB * $pBB;
-
-    my $x = 1 / ($pSAA + $pSAB + $pSBB);
-
-    my $dosage = ($pSAB  + 2 * $pSBB) * $x;
-
-    return $dosage;
-}
-
-sub hardy_weinberg_filter { 
-    my @dosages = @_;
-
-    my %classes = ( AA => 0, AB => 0, BB => 0, NA => 0);
-    
-    foreach my $d (@dosages) { 
-	if (! defined($d)) { 
-	    $classes{NA}++;
-	}
-	elsif ($d >= 0 && $d <= 0.1) { 
-	    $classes{AA}++;
-	}
-	elsif ($d >=0.9 && $d <= 1.1) { 
-	    $classes{AB}++;
-	}
-
-	elsif ($d >=1.9 && $d <= 2.0) { 
-	    $classes{BB}++;
-	}
-	else { 
-	    #print STDERR "Dosage outlier: $d\n";
-	}
-
+for (my $n = 0; $n < @clone_names; $n++) { 
+    if ($clone_info[$n]) { 
+	print $F $clone_names[$n]."\n";
     }
-
-    #print STDERR "NA count: $classes{NA}\n";
- 
-    my $total = $classes{AA} + $classes{AB} + $classes{BB};
-
-    my %score = ();
-    
-    $score{scored_marker_fraction} = $total / @dosages;
-    
-    #print STDERR "AA  $classes{AA}, AB $classes{AB}, BB $classes{BB} Total: $total\n";
-    my $allele_freq = (2 * $classes{AA} + $classes{AB}) / (2 * $total);
-
-    $score{allele_freq} = $allele_freq;
-    
-    my $expected = $allele_freq  * $total;
-    my $x = ($classes{AA} - $expected)**2 / $expected;
-
-    $score{chi} = $x;
-
-    return %score;
 }
-	
-
+close($F);
     
     
     
---
+++ lib/StatFunc.pm
@@ -0,0 +1,115 @@
+
+package StatFunc;
+
+use Math::BigInt;
+
+sub get_counts { 
+    my $snp = shift;
+    
+    my $counts = (split /\:/, $snp)[1];
+
+    my ($c1, $c2) = split /\,/, $counts;
+
+    return ($c1, $c2);
+
+}
+
+sub get_genotype { 
+    my $pAA = shift;
+    my $pAB = shift;
+    my $pBB = shift;
+    my $c1 = shift;
+    my $c2 = shift;
+
+    my $n = $c1 + $c2;
+
+    my $N1 = Math::BigInt->new($n);
+    my $N2 = Math::BigInt->new($n);
+
+ #   print STDERR "$N1 bnok $c1 is: ". $N1->bnok($c1)."\n";
+
+    my $Nbnokc1 = $N1->bnok($c1)->numify();
+    my $Nbnokc2 = $N2->bnok($c2)->numify();
+    
+#    print STDERR "NBnokc1: $Nbnokc1, NBnokc2 $Nbnokc2\n";
+
+    my $pDAA = $Nbnokc1 * (0.99 ** $c1) * (0.01 ** $c2);
+    my $pDAB = $Nbnokc1 * (0.5 ** $c1) * (0.5 ** $c2);
+    my $pDBB = $Nbnokc2 * (0.99 ** $c2) * (0.01 ** $c1);
+
+ #   print STDERR "pDAA: $pDAA pDAB $pDAB, pDBB $pDBB\n";
+
+    my $pSAA = $pDAA * $pAA;
+    my $pSAB = $pDAB * $pAB;
+    my $pSBB = $pDBB * $pBB;
+
+    if ($pSAA + $pSAB + $pSBB == 0) { 
+	return "NA";
+    }
+    
+    my $x = 1 / ($pSAA + $pSAB + $pSBB);
+
+    my $dosage = ($pSAB  + 2 * $pSBB) * $x;
+
+ #   print "DOSAGE: $dosage\n";
+    return $dosage;
+}
+
+sub hardy_weinberg_filter { 
+    my $dosages = shift;
+    my $ignore_clones_count = shift;
+
+    my %classes = ( AA => 0, AB => 0, BB => 0, NA => 0);
+    
+    foreach my $d (@$dosages) { 
+	if (! defined($d)) { 
+	    $classes{NA}++;
+	}
+	elsif ( ($d >= 0) && ($d <= 0.1) ) { 
+	    $classes{AA}++;
+	}
+	elsif ( ($d >=0.9) && ($d <= 1.1) ) { 
+	    $classes{AB}++;
+	}
+
+	elsif (($d >=1.9) && ($d <= 2.0)) { 
+	    $classes{BB}++;
+	}
+	else { 
+	    #print STDERR "Dosage outlier: $d\n";
+	}
+
+    }
+
+    #print STDERR "NA count: $classes{NA}\n";
+ 
+    if ( ( ($classes{AA} ==0) && ($classes{AB} ==0)) ||
+	( ($classes{BB} == 0) && ($classes{AB} ==0)) ) { 
+	    return ( invalid => 1);
+    }
+
+    my $total = $classes{AA} + $classes{AB} + $classes{BB};
+
+    my %score = ();
+    
+    $score{scored_marker_fraction} = $total / (@$dosages - $ignore_clones_count);
+    
+    print STDERR "AA  $classes{AA}, AB $classes{AB}, BB $classes{BB} Total: $total\n";
+    my $allele_freq = (2 * $classes{AA} + $classes{AB}) / (2 * $total);
+
+    $score{heterozygote_count} = $classes{AB};
+
+    $score{allele_freq} = $allele_freq;
+    
+    my $expected = $allele_freq * (1-$allele_freq) * 2 * $total;
+
+    print STDERR "TOTAL: $total\n";
+    my $x = ($classes{AB} - $expected)**2 / $expected;
+
+    $score{chi} = $x;
+
+    return %score;
+}
+	
+
+1;
